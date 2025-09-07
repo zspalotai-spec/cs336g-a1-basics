941b7db13e6fdc373cf8cebcdaa69ecdc8581a12
zspalotai-spec <zspalotai@gmail.com>
2025-09-07 09:08:44 +0200

    format softmax

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -1,10 +1,11 @@
 from einops import reduce
 import torch
 
+
 def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
     x_max, _ = torch.max(x, dim=dim, keepdim=True)
     x_normed = x - x_max
     x_exp = torch.exp(x_normed)
     x_exp_sum = torch.sum(x_exp, dim=dim, keepdim=True)
-    x_out = x_exp/x_exp_sum
+    x_out = x_exp / x_exp_sum
     return x_out
